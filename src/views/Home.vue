bcd3e6c27e6a65e2ba6be52274171176ae733419
xiaoqiu1999 <69064635+xiaoqiu1999@users.noreply.github.com>
2021-04-13 17:28:28 +0800

    安装element ui组件库

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,28 @@
 <template>
-    <h4>You are in home.</h4>
+    <div>
+        <div class="w-1/2 mx-auto">
+            <el-carousel :interval="5000" arrow="always">
+                <el-carousel-item v-for="item in 4" :key="item">
+                    <h3>{{ item }}</h3>
+                </el-carousel-item>
+            </el-carousel>
+        </div>
+    </div>
 </template>
+<style>
+.el-carousel__item h3 {
+    color: #475669;
+    font-size: 18px;
+    opacity: 0.75;
+    line-height: 300px;
+    margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+    background-color: #d3dce6;
+}
+</style>
